95e1d96cdc89ae72f4525d3cacecced2f5a6ee85
JamesGames7 <crask26@pickeringcollege.on.ca>
2025-02-17 11:55:41 -0500

    removing warnings

--- 8349-SwerveDrive/Swerve/src/main/java/frc/robot/RobotContainer.java
+++ 8349-SwerveDrive/Swerve/src/main/java/frc/robot/RobotContainer.java
@@ -13,10 +13,7 @@ import swervelib.SwerveInputStream;
 
 import java.util.function.DoubleSupplier;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
